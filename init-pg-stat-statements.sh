085fae39465fdb62d8da33d24c0e68c7b3954ef4
wojwarych <woj.warych@gmail.com>
2025-06-29 22:47:45 +0200

    add pgstattuple extension

--- init-pg-stat-statements.sh
+++ init-pg-stat-statements.sh
@@ -3,5 +3,6 @@
 echo "enabling pg_stat_statements on database $POSTGRES_DB"
 psql -U $POSTGRES_USER --dbname="$POSTGRES_DB" <<-'EOSQL'
     create extension if not exists pg_stat_statements;
+    create extension if not exists pgstattuple;
 EOSQL
 echo "finished with exit code $?"
