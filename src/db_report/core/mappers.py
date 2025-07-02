12a4edcbac0caec6973bead918339428ca34b070
Wojciech Warych <woj.warych@gmail.com>
2025-07-02 09:04:34 +0000

    added mypy checker; some configuration for pylint; other fixes (#5)

--- src/db_report/core/mappers.py
+++ src/db_report/core/mappers.py
@@ -1,8 +1,12 @@
+"""Module holding mappers for DB to application objects"""
+
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class TablePagesStats:
+class TablePagesStats:  # pylint: disable=too-many-instance-attributes
+    """Class returning stats for table pages"""
+
     table_len: int
     tuple_count: int
     tuple_len: int
@@ -16,6 +20,8 @@ class TablePagesStats:
 
 @dataclass(frozen=True)
 class QueryData:
+    """Class returning stats for particular query"""
+
     query: str
     calls: int
     total_exec_time: int
@@ -25,4 +31,6 @@ class QueryData:
 
 @dataclass(frozen=True)
 class TopQueries:
+    """Class returning list of query stats"""
+
     queries: list[QueryData]
--- src/db_report/storage/engine.py
+++ src/db_report/storage/engine.py
@@ -1,42 +1,69 @@
-from typing import Protocol
+"""Module for providing connection with Storage engine and transactional Session"""
 
+from types import TracebackType
+from typing import Protocol, Self
+
+from dependency_injector import providers
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 
 
 def yield_engine(
     user: str, password: str, host: str, port: int, db_name: str
 ) -> AsyncEngine:
+    """
+    Function for containers.Container to make callable configuration of Singleton engine
+    """
     return create_async_engine(
         f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}",
     )
 
 
 class IUnitOfWork(Protocol):
-    async def __aenter__(self):
+    """Interface definition for creating Unit of Work pattern.
+    Used for communication with DBs for repository (i.e. storage) instances
+    """
+
+    async def __aenter__(self) -> Self:
         return self
 
-    async def __aexit__(self, *args):
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException],
+        exc_value: BaseException | None,
+        traceback: TracebackType,
+    ) -> None:
         await self.rollback()
 
-    async def commit(self): ...
+    async def commit(self) -> None: ...  # pylint: disable=missing-function-docstring
 
-    async def rollback(self): ...
+    async def rollback(self) -> None: ...  # pylint: disable=missing-function-docstring
 
 
 class SQLAlchemyUnitOfWork(IUnitOfWork):
-    def __init__(self, session_factory: AsyncSession):
+    """SQLAlchemy implementation of IUnitoOfWork interface for communicating with DB"""
+
+    def __init__(self, session_factory: providers.Callable[AsyncSession]):
         self.session_factory = session_factory
 
-    async def __aenter__(self):
-        self.session = self.session_factory()
+    async def __aenter__(self) -> Self:
+        """Implements context manager that creates AsyncSession for given context"""
+        self.session = self.session_factory()  # pylint: disable=attribute-defined-outside-init
         return await super().__aenter__()
 
-    async def __aexit__(self, *args):
-        await super().__aexit__(*args)
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException],
+        exc_value: BaseException | None,
+        traceback: TracebackType,
+    ) -> None:
+        """Implements exiting context manager for IUnitOfWork interface"""
+        await super().__aexit__(exc_type, exc_value, traceback)
         await self.session.close()
 
-    async def commit(self):
+    async def commit(self) -> None:
+        """Runs commit method on the session object. Should be used inside ctx manager"""
         await self.session.commit()
 
-    async def rollback(self):
+    async def rollback(self) -> None:
+        """Rollbacks all the changes made inside the session"""
         await self.session.rollback()
